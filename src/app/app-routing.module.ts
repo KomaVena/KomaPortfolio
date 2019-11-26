b3a7eb069626ee478cc48ae760f5423fd309d079
Nikita.Dovnich <n.dovnich@gmail.com>
2019-11-26 13:55:05 +0200

    add new project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,7 +13,6 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'project', component: ProjectComponent },
-  { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent }
 ];
 
